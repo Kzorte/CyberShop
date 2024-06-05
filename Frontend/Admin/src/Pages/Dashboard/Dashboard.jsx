52e653907def164c3a0257ee3b587d604ac4125f
Kzorte <kzorte@gmail.com>
2024-06-05 15:53:27 +0700

    Update Filter Kategori

--- Frontend/Admin/src/Pages/Dashboard/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom'; // Impor Navigate dari react-router-dom
-
-const Dashboard = () => {
-  return <Navigate to="/admin" replace />;
-};
-
-export default Dashboard;
